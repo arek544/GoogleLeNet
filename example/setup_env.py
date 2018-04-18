e00bec795f22d5f7879d10c4c07a6fa92f0aa4f8
Arkadiusz Sycz <arek544@outlook.com>
2018-04-18 19:30:32 +0200

    1

---
+++ example/setup_env.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File: setup_env.py
+# Author: Qian Ge <geqian1001@gmail.com>
+
+import sys
+sys.path.append('../lib/')
+
+SAVE_DIR = 'C:/Users/Arkadiusz/Desktop/GoogLeNet-Inception-tensorflow-master/summary'
+PARA_DIR = 'C:/Users/Arkadiusz/Desktop/GoogLeNet-Inception-tensorflow-master/pre_trained/googlenet.npy'
+DATA_DIR = 'C:/Users/Arkadiusz/Desktop/GoogLeNet-Inception-tensorflow-master/test'
